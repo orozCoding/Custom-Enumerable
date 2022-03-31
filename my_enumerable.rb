5e4d66c3e2a6b921a1bfcefdc9f307ae63be5a52
tYohannes <teklegy@gmail.com>
2022-03-31 16:58:52 +0300

    Add #any? method to MyEnumerable module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -5,4 +5,11 @@ module MyEnumerable
     custom_each { |item| return false unless yield item }
     true
   end
+
+  def any?
+    return 'Block is not given' unless block_given?
+
+    custom_each { |item| return true if yield item }
+    false
+  end
 end
--- my_list.rb
+++ my_list.rb
@@ -17,4 +17,7 @@ class MyList
 end
 
 list = MyList.new(1, 2, 3, 4)
-puts(list.all? {|e| e < 5})
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
